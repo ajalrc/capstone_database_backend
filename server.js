9bf0ff55022452a27974357acae666bc6784be7d
ajalrc <ajalrc456@gmail.com>
2022-04-25 17:56:56 -0500

    make it better

--- server.js
+++ server.js
@@ -8,14 +8,16 @@ app.use(express.json());
 app.use(require("./routes/record"));
 // get driver connection
 const dbo = require("./db/conn");
+const rounter = express.Router();
 
+rounter.get("/", (req, res) => {
+  res.send(`Hello world from the server backend.`);
+});
 
 app.listen(port, () => {
   // perform a database connection when server starts
   dbo.connectToServer(function (err) {
     if (err) console.error(err);
-
   });
   console.log(`Server is running on port: ${port}`);
 });
-
